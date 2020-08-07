a3e7504e117e19aa55f46426e88625a9d31ed8d1
ayobamibincom <ayobami.bincom@gmail.com>
2020-08-07 20:33:21 +0100

    database setup

--- config/database.php
+++ config/database.php
@@ -88,9 +88,15 @@ return [
 
         'pgsql_production' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
 
         ],
 
